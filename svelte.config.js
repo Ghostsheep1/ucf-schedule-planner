74b1951a605c23803b63da5b4bb2fdef63fd1657
Henrique Ribeiro <hsribeiro1@gmail.com>
2026-06-30 16:20:02 -0400

    Create svelte.config.js

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapterNode from "@sveltejs/adapter-node";
+import adapterVercel from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: process.env.VERCEL === "1" ? adapterVercel() : adapterNode(),
+    alias: {
+      $lib: "src/lib"
+    }
+  },
+  preprocess: vitePreprocess()
+};
+
+export default config;
