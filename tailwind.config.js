f7c53ab711957e46eefddd09fa7b6b02b5fff6c3
Henrique Ribeiro <hsribeiro1@gmail.com>
2026-06-30 16:19:59 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: "selector",
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      orange: "#ffc904",
+      lightOrange: "#ffdf70",
+      midGray: "#888888",
+      bgLight: "white",
+      bgSecondaryLight: "#EBEBEB",
+      textLight: "black",
+      divBorderLight: "#F1F1F1",
+      outlineLight: "#A2AABD",
+      secCodesLight: "#667085",
+      hoverLight: "#d8d8d8",
+      bgDark: "#151922",
+      bgSecondaryDark: "#141721",
+      textDark: "#D9DFEA",
+      divBorderDark: "#252E3E",
+      outlineDark: "#47526A",
+      secCodesDark: "#667085",
+      hoverDark: "#30374a",
+      ucfBlack: "#050505",
+      ucfGold: "#ffc904",
+      ucfDarkGold: "#c59100"
+    },
+    extend: {}
+  },
+  plugins: []
+};
